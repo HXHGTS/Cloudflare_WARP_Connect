d00665bc734bd098b7e308fdcdd7df86e98d6e7d
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-04-10 21:04:24 +0800

    Update netflix_support.sh

--- netflix_support.sh
+++ netflix_support.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo 正在安装wireguard. . .
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 curl -o /etc/yum.repos.d/jdoss-wireguard-epel-7.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
 yum install wireguard-dkms wireguard-tools -y
 wget https://github.com/ViRb3/wgcf/releases/download/v2.2.3/wgcf_2.2.3_linux_amd64 -O /etc/wireguard/wgcf_2.2.3_linux_amd64
