56b72211db863367442b95cc545b2dd1e8ad742e
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-04-10 20:58:07 +0800

    Update netflix_support.sh

--- netflix_support.sh
+++ netflix_support.sh
@@ -4,6 +4,7 @@ curl -o /etc/yum.repos.d/jdoss-wireguard-epel-7.repo https://copr.fedorainfraclo
 yum install wireguard-dkms wireguard-tools -y
 wget https://github.com/ViRb3/wgcf/releases/download/v2.2.3/wgcf_2.2.3_linux_amd64 -O /etc/wireguard/wgcf_2.2.3_linux_amd64
 cd /etc/wireguard && chmod +x wgcf_2.2.3_linux_amd64
+echo 162.159.192.1 engage.cloudflareclient.com >> /etc/hosts
 echo 正在注册WARP账号. . .
 ./wgcf_2.2.3_linux_amd64 register
 ./wgcf_2.2.3_linux_amd64 generate
