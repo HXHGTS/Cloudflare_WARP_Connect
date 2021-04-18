0c01df9194ca8f857129d5e86c97a9372d778ba9
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-04-18 08:38:22 +0800

    Update netflix_support.sh

--- netflix_support.sh
+++ netflix_support.sh
@@ -11,5 +11,9 @@ cd /etc/wireguard && chmod +x wgcf_2.2.3_linux_amd64
 echo 正在注册WARP账号. . .
 echo yes | ./wgcf_2.2.3_linux_amd64 register
 ./wgcf_2.2.3_linux_amd64 generate
+echo ##请删除Address = 172.16.0.2/32
+echo ##请删除AllowedIPs = 0.0.0.0/0 >> /etc/wireguard/wgcf-profile.conf
+echo ##请修改engage.cloudflareclient.com为162.159.192.1 >> /etc/wireguard/wgcf-profile.conf
+vi wgcf-profile.conf
 echo 运行成功，请修改配置文件后再运行！
 exit 0
