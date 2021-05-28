9a7082b1981ee989bca13e7e78208c1ab733bc61
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-05-28 18:53:37 +0800

    Update netflix_support.sh

--- netflix_support.sh
+++ netflix_support.sh
@@ -2,6 +2,7 @@
 echo 正在安装wireguard. . .
 echo 13.35.162.73 download.copr.fedorainfracloud.org > /etc/hosts
 echo 38.145.60.24 dl.fedoraproject.org >> /etc/hosts
+yum install net-tools.x86_64 -y
 yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 curl -o /etc/yum.repos.d/jdoss-wireguard-epel-7.repo https://raw.githubusercontent.com/HXHGTS/Add-Netflix_Support/main/jdoss-wireguard-epel-7.repo
 yum install wireguard-dkms wireguard-tools -y
@@ -10,9 +11,8 @@ cd /etc/wireguard && chmod +x wgcf_2.2.3_linux_amd64
 echo 正在注册WARP账号. . .
 echo yes | ./wgcf_2.2.3_linux_amd64 register
 ./wgcf_2.2.3_linux_amd64 generate
-echo ##请删除Address = 172.16.0.2/32 >> /etc/wireguard/wgcf-profile.conf
-echo ##请删除AllowedIPs = 0.0.0.0/0 >> /etc/wireguard/wgcf-profile.conf
-echo ##请修改engage.cloudflareclient.com为162.159.192.1 >> /etc/wireguard/wgcf-profile.conf
-vi wgcf-profile.conf
+echo 请删除Address = 172.16.0.2/32 >> /etc/wireguard/wgcf-profile.conf
+echo 请删除AllowedIPs = 0.0.0.0/0 >> /etc/wireguard/wgcf-profile.conf
+echo 请修改engage.cloudflareclient.com为162.159.192.1 >> /etc/wireguard/wgcf-profile.conf
 echo 运行成功，请修改配置文件后再运行！
 exit 0
