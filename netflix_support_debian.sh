99c07bfb1d7acc862a05d7467462e676ae271cec
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-07-12 11:28:29 +0800

    Update netflix_support_debian.sh

--- netflix_support_debian.sh
+++ netflix_support_debian.sh
@@ -9,9 +9,8 @@ cd /etc/wireguard && chmod +x wgcf_2.2.3_linux_amd64
 echo 正在注册WARP账号. . .
 echo yes | ./wgcf_2.2.3_linux_amd64 register
 ./wgcf_2.2.3_linux_amd64 generate
-echo 请删除Address = 172.16.0.2/32 >> /etc/wireguard/wgcf-profile.conf
-echo 请删除AllowedIPs = 0.0.0.0/0 >> /etc/wireguard/wgcf-profile.conf
-echo 请修改engage.cloudflareclient.com为162.159.192.1 >> /etc/wireguard/wgcf-profile.conf
-systemctl enable wg-quick@wgcf-profile
-echo 运行成功，请修改配置文件后再运行！
+echo /etc/wireguard/wgcf-profile.conf | find -v "172.16.0.2/32"  | find -v "0.0.0.0/0" | find -v "engage.cloudflareclient.com" > /etc/wireguard/wgcf.conf
+echo Endpoint = 162.159.192.1:2408 >> /etc/wireguard/wgcf.conf
+systemctl enable wg-quick@wgcf
+systemctl start wg-quick@wgcf
 exit 0
