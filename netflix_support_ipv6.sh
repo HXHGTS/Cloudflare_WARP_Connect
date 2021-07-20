d20d393eb80b2642081d45e5a24c8381baef80a9
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-07-20 11:25:16 +0800

    Update netflix_support_ipv6.sh

--- netflix_support_ipv6.sh
+++ netflix_support_ipv6.sh
@@ -13,7 +13,7 @@ cd /etc/wireguard && chmod +x wgcf_2.2.3_linux_amd64
 echo 正在注册WARP账号. . .
 echo yes | ./wgcf_2.2.3_linux_amd64 register
 ./wgcf_2.2.3_linux_amd64 generate
-cat /etc/wireguard/wgcf-profile.conf | grep -v "engage.cloudflareclient.com" | grep -v "172.16.0.2/32" | grep -v "0.0.0.0/0" > /etc/wireguard/wgcf.conf
+cat /etc/wireguard/wgcf-profile.conf | grep -v "engage.cloudflareclient.com" | grep -v "/128" | grep -v "::/0" > /etc/wireguard/wgcf.conf
 echo Endpoint = [2606:4700:d0::a29f:c001]:2408 >> /etc/wireguard/wgcf.conf
 systemctl enable wg-quick@wgcf
 systemctl start wg-quick@wgcf
