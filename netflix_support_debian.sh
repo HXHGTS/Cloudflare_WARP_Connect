07870aec716c623648f52d6651438aaf67f69506
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-08-24 19:56:41 +0800

    Update netflix_support_debian.sh

--- netflix_support_debian.sh
+++ netflix_support_debian.sh
@@ -9,7 +9,5 @@ echo yes | ./wgcf_2.2.3_linux_amd64 register
 ./wgcf_2.2.3_linux_amd64 generate
 cat /etc/wireguard/wgcf-profile.conf | grep -v "engage.cloudflareclient.com" | grep -v "172.16.0.2/32" | grep -v "0.0.0.0/0" > /etc/wireguard/wgcf.conf
 echo Endpoint = 162.159.192.1:2408 >> /etc/wireguard/wgcf.conf
-systemctl enable wg-quick@wgcf
-systemctl start wg-quick@wgcf
-echo 运行成功，请修改xray/v2ray配置文件！
+安装成功，请修改xray/v2ray配置文件后手动运行！
 exit 0
