471c5816b77c82f5e7dac749d341ca49a5d31919
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-10-10 14:49:06 +0800

    Update netflix_support_eurserv_debian10.sh

--- netflix_support_eurserv_debian10.sh
+++ netflix_support_eurserv_debian10.sh
@@ -8,11 +8,11 @@ apt install net-tools iproute2 openresolv dnsutils -y
 apt install wireguard-tools --no-install-recommends
 mkdir /etc/wireguard
 curl -sSL https://raw.githubusercontent.com/P3TERX/script/master/wireguard-go.sh | sh
-cd /etc/wireguard && wget https://github.com/ViRb3/wgcf/releases/download/v2.2.3/wgcf_2.2.3_linux_amd64 -O /etc/wireguard/wgcf_2.2.3_linux_amd64
-chmod +x wgcf_2.2.3_linux_amd64
+cd /etc/wireguard && wget https://github.com/ViRb3/wgcf/releases/download/v2.2.8/wgcf_2.2.8_linux_amd64 -O /etc/wireguard/wgcf_2.2.8_linux_amd64
+chmod +x wgcf_2.2.8_linux_amd64
 echo 正在注册WARP账号. . .
-echo yes | ./wgcf_2.2.3_linux_amd64 register
-./wgcf_2.2.3_linux_amd64 generate
+echo yes | ./wgcf_2.2.8_linux_amd64 register
+./wgcf_2.2.8_linux_amd64 generate
 cat /etc/wireguard/wgcf-profile.conf | grep -v "engage.cloudflareclient.com" | grep -v "/128" | grep -v "::/0" > /etc/wireguard/wgcf.conf
 echo Endpoint = [2606:4700:d0::a29f:c001]:2408 >> /etc/wireguard/wgcf.conf
 systemctl enable wg-quick@wgcf
