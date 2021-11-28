7ee67e6e2fb2c60fa291bd98f8e5b8b1f5ab445c
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-11-28 12:58:02 +0800

    Update netflix_support_debian.sh

--- netflix_support_debian.sh
+++ netflix_support_debian.sh
@@ -8,7 +8,20 @@ cd /etc/wireguard && chmod +x wgcf_2.2.8_linux_amd64
 echo 正在注册WARP账号. . .
 echo yes | ./wgcf_2.2.8_linux_amd64 register
 ./wgcf_2.2.8_linux_amd64 generate
-cat /etc/wireguard/wgcf-profile.conf | grep -v "engage.cloudflareclient.com" | grep -v "172.16.0.2/32" | grep -v "0.0.0.0/0" > /etc/wireguard/wgcf.conf
+echo Endpoint = [Interface] > /etc/wireguard/wgcf.conf
+cat /etc/wireguard/wgcf-profile.conf | grep "PrivateKey" >> /etc/wireguard/wgcf.conf
+echo Address = 172.16.0.2/32 >> /etc/wireguard/wgcf.conf
+echo DNS = 1.1.1.1,1.0.0.1 >> /etc/wireguard/wgcf.conf
+echo MTU = 1280 >> /etc/wireguard/wgcf.conf
+echo Table = off >> /etc/wireguard/wgcf.conf
+echo PostUP = ip -4 rule add from 172.16.0.2 lookup 51820 >> /etc/wireguard/wgcf.conf
+echo PostUP = ip -4 route add default dev wgcf table 51820 >> /etc/wireguard/wgcf.conf
+echo PostUP = ip -4 rule add table main suppress_prefixlength 0 >> /etc/wireguard/wgcf.conf
+echo PostDown = ip -4 rule delete from 172.16.0.2/32 lookup 51820 >> /etc/wireguard/wgcf.conf
+echo PostDown = ip -4 rule delete table main suppress_prefixlength 0 >> /etc/wireguard/wgcf.conf
+echo [Peer] >> /etc/wireguard/wgcf.conf
+echo AllowedIPs = 0.0.0.0/0 >> /etc/wireguard/wgcf.conf
+cat /etc/wireguard/wgcf-profile.conf | grep "PublicKey" >> /etc/wireguard/wgcf.conf
 echo Endpoint = 162.159.192.1:2408 >> /etc/wireguard/wgcf.conf
 echo 安装成功，请修改xray/v2ray配置文件后手动运行！
 exit 0
