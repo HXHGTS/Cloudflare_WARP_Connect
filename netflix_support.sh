a7e301521d471c7b3354033e736c0b96e063942d
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-02-08 11:50:46 +0800

    Update netflix_support.sh

--- netflix_support.sh
+++ netflix_support.sh
@@ -6,11 +6,11 @@ yum install net-tools.x86_64 -y
 yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
 curl -o /etc/yum.repos.d/jdoss-wireguard-epel-7.repo https://raw.githubusercontent.com/HXHGTS/Add-Netflix_Support/main/jdoss-wireguard-epel-7.repo
 yum install wireguard-dkms wireguard-tools -y
-wget https://github.com/ViRb3/wgcf/releases/download/v2.2.8/wgcf_2.2.8_linux_amd64 -O /etc/wireguard/wgcf_2.2.8_linux_amd64
-cd /etc/wireguard && chmod +x wgcf_2.2.8_linux_amd64
+wget https://github.com/ViRb3/wgcf/releases/download/v2.2.11/wgcf_2.2.11_linux_amd64 -O /etc/wireguard/wgcf_2.2.11_linux_amd64
+cd /etc/wireguard && chmod +x wgcf_2.2.11_linux_amd64
 echo 正在注册WARP账号. . .
-echo yes | ./wgcf_2.2.8_linux_amd64 register
-./wgcf_2.2.8_linux_amd64 generate
+echo yes | ./wgcf_2.2.11_linux_amd64 register
+./wgcf_2.2.11_linux_amd64 generate
 echo [Interface] > /etc/wireguard/wgcf.conf
 cat /etc/wireguard/wgcf-profile.conf | grep "PrivateKey" >> /etc/wireguard/wgcf.conf
 echo Address = 172.16.0.2/32 >> /etc/wireguard/wgcf.conf
