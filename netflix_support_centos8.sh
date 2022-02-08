259b83c786fcfa11156ed33fa056034000e65b91
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-02-08 11:50:18 +0800

    Update netflix_support_centos8.sh

--- netflix_support_centos8.sh
+++ netflix_support_centos8.sh
@@ -5,11 +5,11 @@ yum install epel-release -y
 yum config-manager --set-enabled PowerTools
 yum copr enable jdoss/wireguard -y
 yum install wireguard-dkms wireguard-tools -y
-wget https://github.com/ViRb3/wgcf/releases/download/v2.2.8/wgcf_2.2.8_linux_amd64 -O /etc/wireguard/wgcf_2.2.8_linux_amd64
-cd /etc/wireguard && chmod +x wgcf_2.2.8_linux_amd64
+wget https://github.com/ViRb3/wgcf/releases/download/v2.2.11/wgcf_2.2.11_linux_amd64 -O /etc/wireguard/wgcf_2.2.11_linux_amd64
+cd /etc/wireguard && chmod +x wgcf_2.2.11_linux_amd64
 echo 正在注册WARP账号. . .
-echo yes | ./wgcf_2.2.8_linux_amd64 register
-./wgcf_2.2.8_linux_amd64 generate
+echo yes | ./wgcf_2.2.11_linux_amd64 register
+./wgcf_2.2.11_linux_amd64 generate
 echo [Interface] > /etc/wireguard/wgcf.conf
 cat /etc/wireguard/wgcf-profile.conf | grep "PrivateKey" >> /etc/wireguard/wgcf.conf
 echo Address = 172.16.0.2/32 >> /etc/wireguard/wgcf.conf
