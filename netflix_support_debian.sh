a1c29eba40f839c41eb869eb56d8911c5fbe6d1d
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-06-21 19:15:33 +0800

    Update netflix_support_debian.sh

--- netflix_support_debian.sh
+++ netflix_support_debian.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set WGCF_VERSION=2.2.18
+WGCF_VERSION=2.2.18
 echo 正在安装wireguard. . .
 apt install lsb-release -y
 echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
@@ -10,7 +10,7 @@ cp -f /etc/resolv.conf.backup /etc/resolv.conf && rm -rf /etc/resolv.conf.backup
 apt install -y linux-headers-$(uname -r)
 apt-get install -y wireguard
 apt-get install -y wireguard-tools --no-install-recommends
-wget https://github.com/ViRb3/wgcf/releases/download/v${WGCF_VERSION}/wgcf_${WGCF_VERSION}_linux_amd64 -O /etc/wireguard/wgcf_${WGCF_VERSION}_linux_amd64
+wget -O /etc/wireguard/wgcf_${WGCF_VERSION}_linux_amd64 https://github.com/ViRb3/wgcf/releases/download/v${WGCF_VERSION}/wgcf_${WGCF_VERSION}_linux_amd64
 cd /etc/wireguard && chmod +x wgcf_${WGCF_VERSION}_linux_amd64
 echo 正在注册WARP账号. . .
 echo yes | ./wgcf_${WGCF_VERSION}_linux_amd64 register
